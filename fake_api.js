60098ca4a5eda88c444e92b8c9bb0e6d47269f48
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-01-25 14:38:40 +0100

    initial commit

---
+++ fake_api.js
@@ -0,0 +1,78 @@
+function getUsers() {
+    return [{
+            "_id": "5c3863c219afdc7c999b7521",
+            "name": "Evangeline Bell",
+            "gender": "female",
+            "company": "PLASMOSIS",
+            "email": "evangelinebell@plasmosis.com",
+            "phone": "+1 (873) 418-3620",
+            "address": "392 Hubbard Street, Hanover, Alaska, 5845",
+            "about": "Excepteur nisi adipisicing quis id officia aute cillum commodo. Ad voluptate voluptate duis mollit officia nostrud laborum deserunt Lorem. Sunt esse qui dolor fugiat ullamco. Sint laborum nulla cupidatat anim laborum consectetur pariatur deserunt culpa ipsum aliqua. Ex qui sit irure sunt est. Esse voluptate in aliqua nulla elit tempor adipisicing exercitation. Laboris commodo cillum dolore ea tempor.\r\n"
+        },
+        {
+            "_id": "5c3863c2d5741f2e4d61cb04",
+            "name": "Aguilar Davenport",
+            "gender": "male",
+            "company": "ZOXY",
+            "email": "aguilardavenport@zoxy.com",
+            "phone": "+1 (931) 508-3592",
+            "address": "414 Barwell Terrace, Winesburg, Guam, 2102",
+            "about": "Eu velit quis officia eiusmod consequat est ad non excepteur ea dolor exercitation aliqua tempor. Eiusmod cillum in non incididunt occaecat anim proident. Sit Lorem velit adipisicing Lorem ut id.\r\n"
+        },
+        {
+            "_id": "5c3863c24bc081366638582c",
+            "name": "Head Daniels",
+            "gender": "male",
+            "company": "KEENGEN",
+            "email": "headdaniels@keengen.com",
+            "phone": "+1 (874) 436-2269",
+            "address": "564 Midwood Street, Warsaw, Federated States Of Micronesia, 8647",
+            "about": "Lorem in ad sint duis minim non incididunt ut ipsum ex reprehenderit aliqua. Nisi reprehenderit reprehenderit non eiusmod esse sint sit culpa cupidatat eiusmod anim. Deserunt magna esse ad consequat occaecat sunt aliqua incididunt et esse est fugiat ut eiusmod.\r\n"
+        },
+        {
+            "_id": "5c3863c24b82dba1780659d1",
+            "name": "Tamera Sanchez",
+            "gender": "female",
+            "company": "ELEMANTRA",
+            "email": "tamerasanchez@elemantra.com",
+            "phone": "+1 (841) 461-2302",
+            "address": "630 Micieli Place, Highland, Utah, 3436",
+            "about": "Laboris amet adipisicing esse commodo enim do aliquip tempor ea Lorem ea adipisicing. Ea id cillum eiusmod officia incididunt fugiat dolore veniam dolor enim commodo. Labore et incididunt consectetur id in deserunt esse officia cillum. Culpa quis quis laborum do dolor ipsum exercitation excepteur enim ex excepteur irure. Commodo proident pariatur qui ex labore exercitation irure quis laboris aliqua esse. Eu enim dolore pariatur nisi fugiat.\r\n"
+        },
+        {
+            "_id": "5c3863c2cda587d7bafc0b29",
+            "name": "Stephens Evans",
+            "gender": "male",
+            "company": "COMVEYOR",
+            "email": "stephensevans@comveyor.com",
+            "phone": "+1 (938) 444-2305",
+            "address": "135 Benson Avenue, Motley, Rhode Island, 3467",
+            "about": "Tempor Lorem consequat voluptate voluptate ea eu. Reprehenderit culpa nulla esse est enim culpa aliquip incididunt cupidatat laboris laboris voluptate ipsum. Non dolor ad ut minim consectetur labore occaecat anim duis veniam mollit nostrud do tempor. Eiusmod culpa sint excepteur nulla mollit adipisicing Lorem et cillum dolor consequat. Sit voluptate mollit dolor mollit in. Tempor incididunt est eu occaecat irure proident magna Lorem minim.\r\n"
+        }
+    ]
+}
+
+function getProducts() {
+    return [
+        {
+            id: 1,
+            name: 'pâte à tartiner',
+            price: 2
+        },
+        {
+            id: 2,
+            name: 'pain complet',
+            price: 1.5
+        },
+        {
+            id: 3,
+            name: 'petit extincteur',
+            price: 19
+        },
+        {
+            id: 4,
+            name: 'vidéo-projecteur',
+            price: 200
+        }
+    ]
+}
